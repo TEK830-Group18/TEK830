05060b39dd92bbf45d2a7926dd725afe984f109e
rezaamani <reza_amani@icloud.com>
2024-09-25 21:56:47 +0200

    Added images to the main frame

--- App.py
+++ App.py
@@ -1,6 +1,8 @@
 from View.AppFrame import AppFrame
+from View.Lamp import Lamp
 
 if __name__ == "__main__":
     app = AppFrame()
+    lamp = Lamp(app)
     app.mainloop()
     
--- View/Lamp.py
+++ View/Lamp.py
@@ -1,13 +1,23 @@
 import tkinter as tk
 
 
-# Bulbs
-bulbOFF = tk.PhotoImage(file = "Images/OFF.jpg")
 
-bulbON = tk.PhotoImage(file = "Images/ON.jpg")
+class Lamp:
+    def __init__(self, parent):
 
-# Swithces
-switchOFF = tk.PhotoImage(file = "Images/SWITCHOFF")
-switchON = tk.PhotoImage(file = "Images/SWITCHON")
+
+        # Bulbs
+        self.bulbOFF = tk.PhotoImage(file = "TEK830/Images/OFF.png")
+        self.bulbON = tk.PhotoImage(file = "TEK830/Images/ON.png")
+
+        # Swithces
+        self.offPic = tk.PhotoImage(file = "TEK830/Images/SWITCHOFF.png")
+        self.btnOFF = tk.Button(parent, image = self.offPic, bd = 0, bg = "white")
+        self.btnOFF.pack(padx=50, pady= 50)
+
+        self.onPic = tk.PhotoImage(file = "TEK830/Images/SWITCHON.png")
+        self.btnON = tk.Button(parent, image = self.onPic, bd = 0, bg = "white")
+        self.btnON.pack(padx=50, pady= 50)
+
 
 
