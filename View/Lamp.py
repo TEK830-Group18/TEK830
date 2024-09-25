ef1cb507862a79091a3f170fa041f33830dffa83
rezaamani <reza_amani@icloud.com>
2024-09-25 14:40:10 +0200

    Added new image to represent the elements of bulb and switch

---
+++ View/Lamp.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+
+# Bulbs
+bulbOFF = tk.PhotoImage(file = "Images/OFF.jpg")
+
+bulbON = tk.PhotoImage(file = "Images/ON.jpg")
+
+# Swithces
+switchOFF = tk.PhotoImage(file = "Images/SWITCHOFF")
+switchON = tk.PhotoImage(file = "Images/SWITCHON")
+
+
