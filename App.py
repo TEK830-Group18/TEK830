3e8708498d4de297101243055a77ea4042ab8682
Noah Andreasson <noahan@student.chalmers.se>
2024-09-26 14:13:48 +0200

    Change App to use multithreading and add Model class

--- App.py
+++ App.py
@@ -1,6 +1,16 @@
 from View.AppFrame import AppFrame
+from model.model import Model
+import threading
 
-if __name__ == "__main__":
+def model():
+    model = Model()
+    model.mainloop()
+
+def view():
     app = AppFrame()
     app.mainloop()
+
+if __name__ == "__main__":
+    modelThread: threading.Thread = threading.Thread(target=model())
+    viewThread: threading.Thread = threading.Thread(target=view())
     
---
+++ model/model.py
@@ -0,0 +1,7 @@
+
+class Model():
+    def __init__(self) -> None:
+        pass
+
+    def mainloop(self):
+        pass
