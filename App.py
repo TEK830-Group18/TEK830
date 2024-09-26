f5aca82e886ab5bcfcec558d69d35dda8d4f16a0
erbergli <erik.berglind@gmail.com>
2024-09-26 09:49:31 +0200

    Add widget for displaying time

--- App.py
+++ App.py
@@ -1,6 +1,10 @@
 from View.AppFrame import AppFrame
+from View.time_widget import TimeWidget
 
 if __name__ == "__main__":
     app = AppFrame()
+    time_widget = TimeWidget(app)
+    time_widget.update_time()
+    
     app.mainloop()
     
--- View/time_widget.py
+++ View/time_widget.py
@@ -1,4 +1,26 @@
 import tkinter as tk
+import time
 
-class TimeWidget():
-    pass
+class TimeWidget(tk.Frame):
+    _time_label: tk.Label
+    def __init__(self, parent):
+        super().__init__(master=parent)
+        _current_time = time.strftime('%H:%M:%S')
+        
+        # Configure row and column
+        self.rowconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)
+        
+        # Create and pack the label
+        self._time_label = tk.Label(self, text=f"{_current_time}", font=("Helvetica", 18))
+        self._time_label.grid(row=0, column=0, padx=10, pady=20)  # Using grid to place the label
+        
+        self.pack(pady=20)
+
+    def update_time(self):
+        current_time = time.strftime('%H:%M:%S')
+        self._time_label.config(text=current_time)
+        self.after(1000,self.update_time)
+        
+        
+        
