864fa0f12643b12331bd117731dca6f1a4960e9d
erbergli <erik.berglind@gmail.com>
2024-09-30 16:08:12 +0200

    Update slider.py

--- View/slider.py
+++ View/slider.py
@@ -3,11 +3,11 @@ import tkinter as tk
 class Slider(tk.Frame):
     def __init__(self, parent):
         super().__init__(master=parent)
+        
         self._seconds:int = 0
         self._minute:int = 0
         self._hour:int = 0
         self.formatted_time = self.format_time(self._hour, self._minute, self._seconds)
-        # self.formatted_time = str(self._hour).zfill(2) + ":" + str(self._minute).zfill(2) + ":" + str(self._seconds).zfill(2)
         
         # init slider
         self._slider = tk.Scale(parent, from_=0, to=1440, orient='horizontal', length=200)
