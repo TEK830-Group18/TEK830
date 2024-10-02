8e8f0dbb7a4020ac40ab2e27cdb39c294f33acd8
Filip Kauffeldt <filip.kauffeldt@gmail.com>
2024-10-02 20:46:43 +0200

    Add Schedule

--- model/schedule.py
+++ model/schedule.py
@@ -1,7 +1,14 @@
-# from typing import List
+from typing import List
+from dataclasses import dataclass
+from model.events.lamp_event import LampEvent
 
-# class Schedule():
-#     def __init__(self) -> None:
-#         self.events: List[int] = []
-#         self.read_data
+@dataclass
+class Schedule:
+    events: List[LampEvent]
+
+    @staticmethod
+    def createSchedule(events: List[LampEvent]) -> 'Schedule':
+        events.sort()
+        return Schedule(events=events)
+
 
