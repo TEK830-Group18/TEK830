979f1f134e7053c27179a49f53604b19dee24f3f
erbergli <erik.berglind@gmail.com>
2024-10-02 15:18:00 +0200

    make it nicer

--- View/time_slider.py
+++ View/time_slider.py
@@ -17,20 +17,20 @@ class TimeSlider(tk.Frame, Observable):
         self.formatted_time = self.format_time(self._hour, self._minute, self._seconds)
         
         # init slider
-        self._slider = tk.Scale(parent, from_=0, to=1440, orient='horizontal', length=500)
+        self._slider = tk.Scale(parent, from_=0, to=1440, orient='horizontal', length=500, showvalue=0)
         self._slider.config(command=self._update_values)
         self._slider.pack()
         
-        # Create and add label to show time
-        self._time_label = tk.Label(text=self.formatted_time)
-        self._time_label.pack()
+        # # Create and add label to show time
+        # self._time_label = tk.Label(text=self.formatted_time)
+        # self._time_label.pack()
     
     def _update_values(self, a):
         self.updating = True
         self._hour = self.get_hours()
         self._minute = self.get_minutes()
         self.formatted_time = self.format_time(self._hour, self._minute, 0)
-        self._time_label.config(text=self.formatted_time)
+        # self._time_label.config(text=self.formatted_time)
         self.notify_observers()
         self.updating = False
         
