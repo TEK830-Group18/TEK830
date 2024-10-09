66f66ae55dfd9d341f4aa2b1d64c241b174079de
Filip Kauffeldt <filip.kauffeldt@gmail.com>
2024-10-09 14:32:54 +0200

    Add ability to set time

--- abstract_timer_service.py
+++ abstract_timer_service.py
@@ -1,9 +1,14 @@
 from abc import ABC, abstractmethod
+from datetime import datetime
 from model.events.lamp_event import LampEvent
 from abstract_light_service import AbstractLightService
 
 class AbstractTimerService(ABC):
     def __init__(self) -> None:
+        pass
+
+    @abstractmethod
+    def setTime(self, time: datetime) -> None:
         pass
 
     @abstractmethod
