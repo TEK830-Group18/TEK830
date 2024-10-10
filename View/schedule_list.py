c4ec75a94122af34dfe62a49c3c07d42e1b0a56c
erbergli <erik.berglind@gmail.com>
2024-10-10 14:00:31 +0200

    Add methods to be implemented in the future

--- View/schedule_list.py
+++ View/schedule_list.py
@@ -4,9 +4,19 @@ class ScheduleList(tk.Frame):
     def __init__(self, parent):
         super().__init__(master=parent)
         
+        self._lbl = tk.Label(self,text="Schedule")
+        self._lbl.pack()
+        
         self._list_box = tk.Listbox(self)
         self._list_box.pack()
         
+        self._insert_schedule()
+        
         self.grid(row=0, column=2, sticky="W",padx=20)
         
-        
+    #TODO Implement this
+    def _insert_schedule(self):
+        """
+        Fills the listbox with entries from a list of scheduled activites.
+        """
+        pass
