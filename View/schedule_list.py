255028192ddbc1a60c5f9f34d1ce654d27491318
erbergli <erik.berglind@gmail.com>
2024-10-11 17:59:47 +0200

    change list from listbox to scrollable frame

--- View/schedule_list.py
+++ View/schedule_list.py
@@ -1,18 +1,36 @@
-import tkinter as tk
+import customtkinter as ctk
 
-class ScheduleList(tk.Frame):
-    def __init__(self, parent):
-        super().__init__(master=parent)
+class ScheduleList(ctk.CTkScrollableFrame):
+    def __init__(self, master, **kwargs):
+        self._width = 150
+        self._height = 230
+        super().__init__(master, **kwargs)
         
-        self._lbl = tk.Label(self,text="SCHEDULE", font=("Helvetica",10,"bold"))
-        self._lbl.pack()
-        
-        self._list_box = tk.Listbox(self, height=15)
-        self._list_box.pack()
-        
-        self._insert_schedule()
+        self.configure(height=self._height, 
+                       width=self._width, 
+                       fg_color = "white", 
+                       label_text = "SCHEDULE", 
+                       label_font = ("Helvetica",16), 
+                       label_text_color = "black",
+                       label_fg_color = "#D3D3D3")
         
         self.grid(row=0, column=2, sticky="W",padx=20)
+
+    
+    
+    
+    # def __init__(self, parent):
+    #     super().__init__(master=parent)
+        
+    #     self._lbl = tk.Label(self,text="SCHEDULE", font=("Helvetica",10,"bold"))
+    #     self._lbl.pack()
+        
+    #     self._list_box = tk.Listbox(self, height=15)
+    #     self._list_box.pack()
+        
+    #     self._insert_schedule()
+        
+    #     self.grid(row=0, column=2, sticky="W",padx=20)
         
     #TODO Implement this
     def _insert_schedule(self):
