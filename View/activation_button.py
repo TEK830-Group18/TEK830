3e0a35e818bbd6fe4d1b6152a2f94483784d71ff
erbergli <erik.berglind@gmail.com>
2024-10-11 17:31:38 +0200

    chacnge button to use CTK

--- View/activation_button.py
+++ View/activation_button.py
@@ -1,21 +1,30 @@
 import tkinter as tk
+import customtkinter as ctk
 
-class ActivationButton(tk.Frame):
+class ActivationButton(ctk.CTkFrame):
     def __init__(self, parent):
         super().__init__(master=parent)
         
         self._activated = False
         
-        self._btn = tk.Button(self, text="Activate HÄRMAPA", bg="#2C2C2C", fg="white", font=("Inter", 16, "bold"), command=self.toggle_modes, width=40)
+        self._btn = ctk.CTkButton(self,
+                                  text="Activate HÄRMAPA", 
+                                  fg_color="#2C2C2C", 
+                                  text_color="white", 
+                                  font=("Inter", 16, "bold"), 
+                                  command=self.toggle_modes, 
+                                  width=300, 
+                                  height=40,
+                                  )
         self._btn.pack(pady=10)
         
         self.grid(row=4, column=1, columnspan=2)
-        self.configure(bg="#0057AD")
+        self.configure(fg_color="#0057AD")
         
     def toggle_modes(self):
         if(self._activated == False):
-            self._btn.config(text="Deactivate HÄRMAPA")
+            self._btn.configure(text="Deactivate HÄRMAPA")
             self._activated = True
         else:
-            self._btn.config(text="Activate HÄRMAPA")
+            self._btn.configure(text="Activate HÄRMAPA")
             self._activated = False
