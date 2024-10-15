ce3363bdf794e6b92acddf98e8e43d3be7e0a274
Flaskhals68 <03kajo@gmail.com>
2024-10-15 16:11:13 +0200

    Add LSTM model

--- model/nn_model/nn_model.py
+++ model/nn_model/nn_model.py
@@ -4,8 +4,11 @@ import torch.nn.functional as F
 class NNModel(nn.Module):
     def __init__(self, input_size, hidden_size):
         super(NNModel, self).__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size)
-        self.fc2 = nn.Linear(hidden_size, 2)  # Output layer for binary classification (on/off)
+        # self.fc1 = nn.Linear(input_size, hidden_size)
+        # self.fc2 = nn.Linear(hidden_size, 2)  # Output layer for binary classification (on/off)
+        self.fc1 = nn.Linear(input_size, 64)
+        self.lstm = nn.LSTM(input_size, hidden_size, batch_first=True)
+        self.hiddenToAction = nn.Linear(hidden_size, 2)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
