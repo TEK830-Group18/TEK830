116fbd1ef36a2c679528a4132d0a7aae12a09b7b
Flaskhals68 <03kajo@gmail.com>
2024-10-16 23:11:33 +0200

    Rework NNScheduler and NNModel

--- model/nn_model/nn_model.py
+++ model/nn_model/nn_model.py
@@ -1,17 +1,17 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+    
 class NNModel(nn.Module):
-    def __init__(self, input_size, hidden_size):
+    def __init__(self, input_size):
         super(NNModel, self).__init__()
-        # self.fc1 = nn.Linear(input_size, hidden_size)
-        # self.fc2 = nn.Linear(hidden_size, 2)  # Output layer for binary classification (on/off)
         self.fc1 = nn.Linear(input_size, 64)
-        self.lstm = nn.LSTM(input_size, hidden_size, batch_first=True)
-        self.hiddenToAction = nn.Linear(hidden_size, 2)
+        self.fc2 = nn.Linear(64, 32)
+        self.fc3 = nn.Linear(32, 1)
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        x = F.relu(self.fc1(x))
-        out = self.fc2(x)
-        return out
-    
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = self.sigmoid(self.fc3(x))
+        return x
