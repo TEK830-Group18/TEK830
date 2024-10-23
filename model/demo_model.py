927fbcf1c5621787883cd60b81d4015df387bb34
erbergli <erik.berglind@gmail.com>
2024-10-23 14:44:32 +0200

    now it should work

--- model/demo_model.py
+++ model/demo_model.py
@@ -30,8 +30,10 @@ class DemoModel(Model):
 
         self.current_time_in_minutes = self.current_time.hour * 60 + self.current_time.minute
             
-        self.active_lamps_list = self.create_active_lamp_list()
-        self.currently_active_lamps = self.active_lamps_list[self.current_time_in_minutes]
+        self.current_active_lamps_list = self.create_active_lamp_list()
+        self.currently_active_lamps = self.current_active_lamps_list[self.current_time_in_minutes]
+        self.prev_current_active_lamps_list = self.current_active_lamps_list
+
         
         # to turn on lamps that should be turned on, if given a start time that means that lights should be on
         for lamp in self.currently_active_lamps:
@@ -51,7 +53,7 @@ class DemoModel(Model):
         time_int = self._get_minutes_from_datetime(time)
         prev_active_lamps = self.currently_active_lamps
         self.timer.set_time(time)
-        self.currently_active_lamps = self.active_lamps_list[time_int]
+        self.currently_active_lamps = self.current_active_lamps_list[time_int]
         
         if len(self.currently_active_lamps) == 0:
             for lamp in prev_active_lamps:
@@ -89,15 +91,15 @@ class DemoModel(Model):
             t.sleep(1)
             self.current_time = self.get_time()
             current_time_in_minutes = self._get_minutes_from_datetime(self.current_time)
-            self.currently_active_lamps = self.active_lamps_list[current_time_in_minutes]
+            self.currently_active_lamps = self.current_active_lamps_list[current_time_in_minutes]
             
             prev_time_in_minutes = self._get_minutes_from_datetime(self.prev_time)
-            prev_active_lamps = self.active_lamps_list[prev_time_in_minutes]
+            prev_active_lamps = self.current_active_lamps_list[prev_time_in_minutes]
             
             lamps_to_update = []
             if self.current_time.minute > self.prev_time.minute and prev_active_lamps != self.currently_active_lamps:
                 
-                lamps_to_update = list(set(prev_active_lamps).symmetric_difference(set(self.active_lamps_list[current_time_in_minutes])))
+                lamps_to_update = list(set(prev_active_lamps).symmetric_difference(set(self.current_active_lamps_list[current_time_in_minutes])))
                 print(F"{lamps_to_update} are to be updated")
                 
                 if len(lamps_to_update) == 0:
@@ -116,7 +118,7 @@ class DemoModel(Model):
         return time.hour * 60 + time.minute
 
     def get_schedule(self):
-        user_data = "tools\mock_user_data.json"
+        user_data = "tools\mock_user_data copy.json"
         user_events = self.read_data(user_data)
         return self.scheduler.createSchedule(user_events)
     
@@ -154,33 +156,39 @@ class DemoModel(Model):
         return self.current_schedule
     
     def change_current_schedule(self):
-        prev_active_lamps_list = self.active_lamps_list
         current_time_in_minutes = self._get_minutes_from_datetime(self.get_time())
 
         if not self.use_using_schedule:
+            print("use user schedule")
             self.current_schedule = self.user_schedule
-            self.publish_events_after_schedule_switch(current_time_in_minutes, prev_active_lamps_list)
+            self.publish_events_after_schedule_switch(current_time_in_minutes, self.prev_current_active_lamps_list)
             self.use_using_schedule = True
         else:
+            print("use algorithm schedule")
             self.current_schedule = self.algortihm_schedule
-            self.publish_events_after_schedule_switch(current_time_in_minutes, prev_active_lamps_list)
+            self.publish_events_after_schedule_switch(current_time_in_minutes, self.prev_current_active_lamps_list)
             self.use_using_schedule = False
+        self.prev_current_active_lamps_list = self.current_active_lamps_list
 
 
-    def publish_events_after_schedule_switch(self, current_time_in_minutes, prev_active_lamps_list):
-        new_active_lamps_list = self.create_active_lamp_list()
+    def publish_events_after_schedule_switch(self, current_time_in_minutes, prev_current_active_lamps_list):
+        new_current_active_lamps_list = self.create_active_lamp_list()
+        print(len(new_current_active_lamps_list[current_time_in_minutes]) == 0)
+        print(prev_current_active_lamps_list[current_time_in_minutes])
         
-        if len(new_active_lamps_list[current_time_in_minutes]) == 0:
-            for lamp in prev_active_lamps_list[current_time_in_minutes]:
+        if len(new_current_active_lamps_list[current_time_in_minutes]) == 0:
+            for lamp in prev_current_active_lamps_list[current_time_in_minutes]:
                 self.publish_lamp_event(self.get_time(),lamp,LampAction.OFF)
-            return
-        if prev_active_lamps_list[current_time_in_minutes] != new_active_lamps_list[current_time_in_minutes]:
-            lamps_to_update = list(set(prev_active_lamps_list[current_time_in_minutes]).symmetric_difference(set(new_active_lamps_list[current_time_in_minutes])))
+            
+        if prev_current_active_lamps_list[current_time_in_minutes] != new_current_active_lamps_list[current_time_in_minutes]:
+            lamps_to_update = list(set(prev_current_active_lamps_list[current_time_in_minutes]).symmetric_difference(set(new_current_active_lamps_list[current_time_in_minutes])))
             for lamp in lamps_to_update:
-                if lamp in prev_active_lamps_list[current_time_in_minutes]:
+                if lamp in prev_current_active_lamps_list[current_time_in_minutes]:
                     self.publish_lamp_event(self.get_time(),lamp,LampAction.OFF)
                 else:
                     self.publish_lamp_event(self.get_time(),lamp,LampAction.ON)
+        self.current_active_lamps_list = new_current_active_lamps_list
+
                     
     def read_data(self, path:str):
         with open(path, mode='r') as f:
